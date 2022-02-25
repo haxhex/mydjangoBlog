3f048e7ee3e9125c6ffe93213c89712cbdfa1392
Helia Ghorbani <ghorbani3452002@yahoo.com>
2022-02-25 04:14:33 +0330

    add url parameters

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from . import views
 # . : current path
+# <> : variable, capture url in views
 
+app_name = "articles"
 urlpatterns = [
-    path('', views.article_list)
+    path('', views.article_list, name="list"),
+    path('<slug>', views.article_detail, name="detail"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from . import models
 
 # Create your views here.
@@ -10,3 +10,6 @@ def article_list(request):
     # access articles from html file by using dict of articles in render
     args = {'articles' : articles}
     return render(request, 'articles/articleslist.html', args)
+
+def article_detail(request, slug):
+    return HttpResponse(slug)
