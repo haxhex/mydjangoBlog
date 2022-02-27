50a3080e8846e926be25644336dad24135c0893b
Helia Ghorbani <ghorbani3452002@yahoo.com>
2022-02-27 04:51:34 +0330

    add minor changes

--- articles/views.py
+++ articles/views.py
@@ -6,7 +6,8 @@ from . import models
 def article_list(request):
     # store all objects in Article in articles
     # sort according to date order_by()
-    articles = models.Article.objects.all().order_by('date')
+    # (-) order discending
+    articles = models.Article.objects.all().order_by('-date')
     # access articles from html file by using dict of articles in render
     args = {'articles' : articles}
     return render(request, 'articles/articleslist.html', args)
