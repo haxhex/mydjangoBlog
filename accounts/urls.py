e824344e0ad8f8084bf13a34be632a92bff02706
Helia Ghorbani <ghorbani3452002@yahoo.com>
2022-02-28 22:55:50 +0330

    log user in

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'accounts'
 # path for sign up
 urlpatterns = [
-    path('signup', views.signup_view, name='signup')
+    path('signup', views.signup_view, name='signup'),
+    path('login', views.login_view, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 
 def signup_view(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        # value in form get from POST (like http)
+        # if user click on submit make a form with filled value
+        # save in database
+        form = UserCreationForm(request.POST)
+        # if values are valid
+        if form.is_valid():
+            # get users details
+            user = form.save()
+            login(request, user)
+            # login
+            # redirect to another page after sign up
+            return redirect('articles:list')
+    else:
+            # if method is not 'POST' means it is 'GET'
+            # Create new form for user
+            # After click 'submit' first time the return to this method see
+            # this time method is 'POST' so if execute
+        form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form' : form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # login user
+            user = form.get_user()
+            login(request, user)
+            return redirect('articles:list')
+    else:
+        # method 'GET'
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form' : form})
