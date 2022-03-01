ed598b0aa0eee96fdb7f32a6d7b35573e347ec45
Helia Ghorbani <ghorbani3452002@yahoo.com>
2022-03-01 04:57:42 +0330

    add styles

--- djangoBlog/urls.py
+++ djangoBlog/urls.py
@@ -4,15 +4,16 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
+from articles.views import article_list
 # . : current path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
-    path('', views.home),
     # if we go to article next urls will be in this path
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
+    path('', article_list),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
